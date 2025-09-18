f76ac2a216c9860f386e0b4acdb18691c655033f
Clos3r <sanioksasha777@gmail.com>
2025-09-18 09:58:29 +0300

    calc

---
+++ lab3.ipynb.py
@@ -0,0 +1,23 @@
+def calculator():
+    num1 = float(input("Введіть перше число: "))
+    operator = input("Введіть оператор (+, -, *, /): ")
+    
+    num2 = float(input("Введіть друге число: "))
+    
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Помилка: ділення на нуль!"
+    else:
+        result = "Невідомий оператор!"
+    
+    print(f"Результат: {result}")
+
+calculator()
